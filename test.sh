da0c223589d675249a2875b506491bfd1044f368
Robert Peters <robert.peters@fabmatics.com>
2023-12-23 22:48:28 +0100

    test: Move rust test script into rust directory

--- test.sh
+++ test.sh
@@ -21,25 +21,6 @@ run_zig_int_test() {
 	fi
 }
 
-run_rst_unit_test() {
-	cargo -C "$(pwd)/rust/" -Z unstable-options test
-}
-
-run_rst_int_test() {
-	day="${1}"
-	part="${2}"
-	input="${3}"
-	expected="${4}"
-
-	echo "run integration test day:${day} part:${part}"
-	result=$(cargo -C "$(pwd)/rust" -Z unstable-options run --release -- "${day}" "${part}" "${input}")
-	if [[ "${result}" == "${expected}" ]]; then
-		echo "  OK";
-	else
-		echo "  FAIL ('${result}' != '${expected}')";
-	fi
-}
-
 #pushd zig
 #run_zig_unit_test "src/day1.zig"
 #run_zig_unit_test "src/day3.zig"
@@ -62,15 +43,3 @@ run_rst_int_test() {
 #run_zig_int_test 12 1 "../inputs/day12_part1.txt" 7173
 #run_zig_int_test 15 1 "../inputs/day15_part1.txt" 510801
 #popd
-run_rst_unit_test
-run_rst_int_test 2 1 "../inputs/day2_part1.txt" 2600
-run_rst_int_test 2 2 "../inputs/day2_part1.txt" 86036
-run_rst_int_test 3 1 "../inputs/day3_part1.txt" 517021
-run_rst_int_test 3 2 "../inputs/day3_part1.txt" 81296995
-run_rst_int_test 16 1 "../inputs/day16_part1.txt" 7517
-run_rst_int_test 16 2 "../inputs/day16_part1.txt" 7741
-run_rst_int_test 19 1 "../inputs/day19_part1.txt" 353046
-run_rst_int_test 19 2 "../inputs/day19_part1.txt" 125355665599537
-run_rst_int_test 20 1 "../inputs/day20_part1.txt" 807069600
-run_rst_int_test 22 1 "../inputs/day22_part1.txt" 454
-run_rst_int_test 22 2 "../inputs/day22_part1.txt" 74287
